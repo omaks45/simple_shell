a0c9cac22f744743780c56dd6790ab8fc6e4d651
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-09-07 23:17:15 +0100

    atoi.c

--- atoi.c
+++ atoi.c
@@ -1,23 +1,13 @@
 #include "shell.h"
 
-
-
 /**
-
  * interactive - returns true if shell is interactive mode
-
  * @info: struct address
-
  *
-
  * Return: 1 if interactive mode, 0 otherwise
-
  */
-
 int interactive(info_t *info)
-
 {
-
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
@@ -29,8 +19,7 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
-
-		while (*delim)
+	while (*delim)
 		if (*delim++ == c)
 			return (1);
 	return (0);
@@ -44,7 +33,6 @@ int is_delim(char c, char *delim)
 
 int _isalpha(int c)
 {
-
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
@@ -52,14 +40,13 @@ int _isalpha(int c)
 }
 
 /**
- *atoi - converts a string to an integer
+ *_atoi - converts a string to an integer
  *@s: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
 int _atoi(char *s)
 {
-
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
@@ -67,14 +54,12 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			sign *= -1;
+
 		if (s[i] >= '0' && s[i] <= '9')
-
 		{
-
 			flag = 1;
 			result *= 10;
 			result += (s[i] - '0');
-
 		}
 		else if (flag == 1)
 			flag = 2;
@@ -82,9 +67,8 @@ int _atoi(char *s)
 
 	if (sign == -1)
 		output = -result;
-
 	else
 		output = result;
 
-	return (output)
+	return (output);
 }
